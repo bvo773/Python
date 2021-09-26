09650ad4d7e68db601d687747ff2052b2b17f870
Binh Vo <binhvo07@gmail.com>
2021-09-25 20:10:36 -0500

    test

--- Python100Days/day5/main.py
+++ Python100Days/day5/main.py
@@ -142,15 +142,15 @@ Fizz
 FizzBuzz`
 '''
 def fizzBuzz():
-	for number in range(1,101):
-		if (number % 3 == 0) and (number % 5 == 0):
-			print('FizzBuzz')
-		elif number % 3 == 0:
-			print('Fizz')
-		elif number % 5 == 0:
-			print('Buzz')
-		else:
-			print (number)
+ for number in range(1,101):
+	 if (number % 3 == 0) and (number % 5 == 0):
+		 print('FizzBuzz')
+	 elif number % 3 == 0:
+		 print('Fizz')
+	 elif number % 5 == 0:
+		 print('Buzz')
+	 else:
+		 print (number)
 
 def helloworld():
 	print("hello world")
