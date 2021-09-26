ef0a4c0b6dc7a8ad9e09eb6878e58ffa8440e622
Binh Vo <binhvo07@gmail.com>
2021-09-25 20:15:03 -0500

    test

--- Python100Days/day4/projectday4.py
+++ Python100Days/day4/projectday4.py
@@ -40,28 +40,28 @@ userNumChoice = int(input("What do you choose? Type 0 for ROCK, 1 for PAPER, 2 f
 cpuNumChoice = random.randint(0, len(stringChoices)-1)
 
 if (userNumChoice < 0 or userNumChoice > 2):
-		print("Invalid choice, you lose!")
+	print("Invalid choice, you lose!")
 else:
-		userChoice = stringChoices[userNumChoice]	
-		cpuChoice = stringChoices[cpuNumChoice]
+	userChoice = stringChoices[userNumChoice]	
+	cpuChoice = stringChoices[cpuNumChoice]
 
-		asciiUser = asciiChoices[userNumChoice]
-		asciiCpu = asciiChoices[cpuNumChoice]
+	asciiUser = asciiChoices[userNumChoice]
+	asciiCpu = asciiChoices[cpuNumChoice]
 
-		rockCheck = stringChoices[0]
-		paperCheck = stringChoices[1]
-		scissorsCheck = stringChoices[2]
+	rockCheck = stringChoices[0]
+	paperCheck = stringChoices[1]
+	scissorsCheck = stringChoices[2]
 
-		if userChoice == cpuChoice:
-				print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n DRAW!")
+	if userChoice == cpuChoice:
+		print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n DRAW!")
+	else:
+		if (userChoice == rockCheck) and (cpuChoice == scissorsCheck):
+			print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU WIN!")
+		elif (userChoice == scissorsCheck) and (cpuChoice == paperCheck):
+			print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU WIN!")
+		elif (userChoice == paperCheck) and (cpuChoice == rockCheck):
+			print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU WIN!")
 		else:
-			if (userChoice == rockCheck) and (cpuChoice == scissorsCheck):
-				print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU WIN!")
-			elif (userChoice == scissorsCheck) and (cpuChoice == paperCheck):
-				print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU WIN!")
-			elif (userChoice == paperCheck) and (cpuChoice == rockCheck):
-				print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU WIN!")
-			else:
-				print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU LOSE")
+			print(f"\nUSER CHOICE:\n {asciiUser}\n\nCPU CHOICE:\n {asciiCpu}\n YOU LOSE")
 
 
