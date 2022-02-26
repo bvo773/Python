0dbcbba8372c15bcef6f79bc0206bd650f47ebac
Binh Vo <binhvo07@gmail.com>
2022-02-26 07:49:03 -0600

    revised day 3,4

--- Python100Days/day4/main.py
+++ Python100Days/day4/main.py
@@ -20,7 +20,7 @@ NOTES
 '''		
 
 '''
-39 - RANDOM MODULE
+41 - RANDOM MODULE (Pseudrandom number generators)
 Module example car - different modules (engine, wheel, axles, etc)
 module helps divide the code into different functionalities
 '''
@@ -28,7 +28,7 @@ def importingModule():
 	import my_module
 	print(my_module.pi) # importing my module and acess the pi variable
 
-
+# Importing random module
 def randomModule():
 	import random
 	randomInteger = random.randint(1,5) # [0,2] inclusive
@@ -37,9 +37,12 @@ def randomModule():
 	randomFloat = random.random()  #returns x within interval [0,1) 0.00000 - 0.99999
 	print(randomFloat)
 
-#random decimal between 0 and 5 using random(): 0.000... - 4.9999..
-	decimal = randomFloat * 5
-	print ("Decimal: {:.2f}".format(decimal))
+  #to get float number multiply it with an intenger
+  #random decimal between 0 and 5 using random(): 0.000... - 4.9999..
+	randomFloat = randomFloat * randomInteger
+	print ("Random Float: {:.2f}".format(randomFloat))
+
+randomModule()
 
 # instead of matching their names to TRUE or LOVE lettters
 def loveScore():
@@ -47,7 +50,7 @@ def loveScore():
 	print(f"Your love score is {random.randint(1,100)}")
 
 '''
-40 - RANDOM EXERCISE
+42 - [Interacting Coding Exercise] RANDOM EXERCISE
 A virtual coin toss program. It will randomly tell the user "Heads" or "Tails"
 '''
 def coinToss():
@@ -63,28 +66,27 @@ def coinToss():
 			print(" Tails")
 
 '''
-41 - List
+43 - List
 DOC: https://docs.python.org/3/tutorial/datastructures.html
-A list is data structure.
-Data Structures: are methods or ways to organize and store data efficiently
+A list is data structure. It has methods or ways to organize and store data efficiently
 A variable can only store a single piece of data (int, string)
 A data structure can store grouped pieces of data (data that has relation or connection to each other)
 Eg) Store all the names of the US States together as one
 
 Python List - A set of square brackets [] with many items stored inside. The items can be any data type.
 It can even have a mixed data types [store strings, numbers, or set of booleans]
-			index				0 		 1      2
-	Eg) fruits = | [item1, item2, item3] 
-						(begin)
-			- item1 is at the beginning of the list (offset or a shift of 0)
-			- item2 is shifted from the beginning by 1 
-			- item3 is shifted from the beginning by 2
+			index				   0 		    1        2
+	Eg) fruits = | ["cherry", "apple", "pear"] 
+						     (begin)
+			- cherry is at the beginning of the list (offset or a shift of 0)
+			- apple is 'shifted' from the beginning by 1 
+			- pear is 'shifted' from the beginning by 2
 '''
 def pythonList():
 	usStates = ["California", "Illinois","Delaware","New York","Connecticut"]
 	print(f'First element in list: {usStates[0]}') # first item
 	print(f'Last element in the list: {usStates[-1]}') # last item in the last
-	print(f'Second to last element in the list {usStates[-2]}')
+	print(f'Second to last element in the list {usStates[-2]}') # the  before the last item
 
 	# Modify list to set a new piece of data
 	usStates[0] = "Oregon" # At index 0, oregon is the new data 
@@ -110,11 +112,13 @@ def bankerRoulete():
 	print(f"{person_who_paid} is going to pay the Bill!")
 
 '''
-43 - IndexErrors and working with nested lists
+44 - IndexErrors and working with nested lists
 		0      1
 [item1,item2][2]  -> IndexError: list index out of range
 
 TIP:use len() to check the size of the list
+
+Nested lists - list within a list
 '''
 def hightPesticidesFood():
 	'''dirtyDozen = ["Strawberries", "Spinach", "Kale", "Nectaries", "Apples", "Grapes",
@@ -136,7 +140,7 @@ def hightPesticidesFood():
 	print(dirtyDozen[1][3])
 
 '''
-41 - Treasure Map exercise: https://replit.com/@BinhVo2/day-4-3-exercise#README.md
+46 - Treasure Map exercise: https://replit.com/@BinhVo2/day-4-3-exercise#README.md
 Row 3, Column 2 would be entered as: 32
 ['⬜️', '⬜️', '⬜️']
 
@@ -156,8 +160,8 @@ def treasureMap():
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-	row = int(str(position[0])) - 1
-	column = int(str(position[1])) - 1
+	column = int(position[0]) - 1
+	row = int(position[1]) - 1
 	map[row][column] = "X"
 
 #selected_row = map[row]
@@ -176,12 +180,64 @@ def treasureMapYu():
 	print(f"{row1}\n{row2}\n{row3}")
 	position = input("Where do you want to put the treasure? (Column,Row) eg. 23: ")
 
-	column = int(str(position[0])) - 1
-	row = int(str(position[1])) - 1
+	column = int(position[0]) - 1
+	row = int(position[1]) - 1
 
 	# python understand multidimensioanl list by [row,col] 
 	map[row][column] = "X"
 
 	print(f"{map[0]}\n{map[1]}\n{map[2]}")
 
-treasureMapYu()
+def rockPaperScissors():
+  rock = '''
+      _______
+  ---'   ____)
+        (_____)
+        (_____)
+        (____)
+  ---.__(___)
+  '''
+
+  paper = '''
+      _______
+  ---'   ____)____
+            ______)
+            _______)
+          _______)
+  ---.__________)
+  '''
+
+  scissors = '''
+      _______
+  ---'   ____)____
+            ______)
+        __________)
+        (____)
+  ---.__(___)
+  '''
+
+  #Write your code below this line 👇
+  import random
+  resultList = [rock, paper, scissors]
+  cpuChoice = random.randint(0,2)
+  userChoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+
+  print(resultList[userChoice])
+  print(f"Computer chose: \n{resultList[cpuChoice]}")
+  # user - rock, cpu paper
+  if userChoice == 0 and cpuChoice == 1: 
+    print("You lose!")
+  elif userChoice == 0 and cpuChoice == 2:
+    print("You win!")
+  elif userChoice == 1 and cpuChoice == 0:
+    print("You win!")
+  elif userChoice == 1 and cpuChoice == 2:
+    print("You lose!")
+  elif userChoice == 2 and cpuChoice == 0:
+    print("You lose!")
+  elif userChoice == 2 and cpuChoice == 1:
+    print("You win!")
+  else:
+    print("Draw!")
+
+rockPaperScissors()
